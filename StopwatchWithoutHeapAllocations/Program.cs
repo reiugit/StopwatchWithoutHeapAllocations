a54fa1a90a4c8e1d795e230bc2ec68506fec91d9
reiugit <67372053+reiugit@users.noreply.github.com>
2024-10-30 07:25:54 +0100

    Projektdateien hinzufügen.

---
+++ StopwatchWithoutHeapAllocations/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+// Avoiding the instantiation of the Stopwatch class with the static GetTimestamp method
+
+var startingTimestamp = Stopwatch.GetTimestamp();
+{
+    await Task.Delay(500);
+}
+var elapsed = Stopwatch.GetElapsedTime(startingTimestamp);
+
+Console.WriteLine($"\nElapsed Time:  {elapsed}");
